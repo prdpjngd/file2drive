7e39b9325761cc066c537a6f2b749817627765cd
pradeep <pradeepjangid.jky@gmail.com>
2020-01-18 15:48:44 +0530

    1st push

--- index.py
+++ index.py
@@ -5,3 +5,9 @@ def home():
     return render_template('index.html')
 if __name__ == '__main__':
     app.run(debug=True)
+
+@app.route('/auth')
+def home():
+    return render_template('index.html')
+if __name__ == '__main__':
+    app.run(debug=True)
